a615897299ccbfa4f9d58d8b175b959502b3be78
samar-jeet <81514487+samar-jeet@users.noreply.github.com>
2023-06-13 14:29:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# mental_health
-n our project we are preparing a ML model that predicts the level of stress an individual bear. And accordingly we are detecting the crucial factors causing stress to them and hence providing personalized solutions depending on the factors affecting the mental health of the user.
+
+**Hack 36 6.0**
+
+![built-at-hack](https://user-images.githubusercontent.com/76893116/227752940-5a04514f-9909-4a1b-aba6-52978baa72fc.jpg)
+
+# Chill_Down
+
+ Team Name: **init\_0**
+<h5>Jagdish Kumar</h5>
+<h5>Priyansh Verma </h5>
+<h5>Ashish Nishad</h5>
+<h5>Abhi Bhaskar </h5>
+
+Video link : https://drive.google.com/file/d/13gdsnkh4SijeM_z0srKVJ8gDTGiYQzFy/view?usp=drivesdk
+<hr>
+PPT link: https://drive.google.com/file/d/1ra3ic2SV5KAF3FX3yLBfMA0uyVq2sbkb/view?usp=drivesdk
+
+ **Project name: Chill-Down**
+
+**Description: Mental Health Improvement by detecting stress inducing factor**
+
+In our project we  are preparing a ML model that predicts the level of stress an individual bear. And accordingly we are detecting the crucial factors causing stress to them and hence providing personalized solutions depending on the factors affecting the mental health of the user.
+
+**Features:** 
+
+- Collecting data by Self Assessment
+- Predicting the level of Stress
+- Finding out the major factors that are inducing Stress.
+- Giving Personalized Solutions accordingly
+- Real life motivational stories that connect the user’s sentiments (recommending system)
+- For Critical Users(highly stressed) we recommend Psychologists.
+- Chatbot for general Queries related to mental health of user.
+
+![Screenshot (40) (1)](https://user-images.githubusercontent.com/76893116/227753335-ea4c650f-00c5-4ca2-b1b7-8f1ee2722d81.png)
+
+![Screenshot (38)](https://user-images.githubusercontent.com/76893116/227753180-bce722df-4ec5-4a38-9d37-dac5d78dfd96.png)
+![Screenshot (39)](https://user-images.githubusercontent.com/76893116/227753233-af435691-b11e-4947-b44a-2bdefcd2991c.png)
+
+
+Login Page
+
+![image](https://user-images.githubusercontent.com/76893116/227752981-98e2edb3-ccdd-44cc-82bd-af42d09fac36.png)
+![image](https://user-images.githubusercontent.com/76893116/227752973-bc48d5d9-7f4c-4b42-b1d4-425bdf9db50f.png)
+![image](https://user-images.githubusercontent.com/76893116/227752971-05aeaf38-dc33-413f-99f7-9b263f543369.png)
+![image](https://user-images.githubusercontent.com/76893116/227753043-f9880a8c-f2d4-4586-86da-0fb317ae17ba.png)
+
+![image](https://user-images.githubusercontent.com/76893116/227752978-ba6f41aa-f121-4bcf-963e-3890567f0c2b.png)
+
+
+
+![built-at-hack](https://user-images.githubusercontent.com/76893116/227752941-dcbf10e3-0dd4-40c7-a5ab-e17cfab43e34.jpg)
---
+++ frontend/src/assets/data/data.js
@@ -0,0 +1,97 @@
+
+export const BLOG__DATA = [
+  {
+    id: "01",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "02",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "03",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "04",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "01",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "05",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "06",
+    title: "Stay in Resistance Monkey ",
+    category:"dummy",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "07",
+    title: "Stay in Resistance Monkey ",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  
+  {
+    id: "08",
+    title: "Stay in Resistance Monkey ",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  {
+    id: "09",
+    title: "Stay in Resistance Monkey ",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+  {
+    id: "010",
+    title: "Stay in Resistance Monkey ",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
+    creator: "Priyansh Verma",
+   
+  },
+
+];
+
